a899616202f3b950f83bd536ef0c8f5b0b66d976
Adamor Henner <adamor.henner@boavistatecnologia.com.br>
2023-08-12 15:08:06 -0300

    feat: Criando os Clients do OAuth2.

--- auth-server/src/main/java/com/algaworks/example/auth/server/security/AuthSecurityConfig.java
+++ auth-server/src/main/java/com/algaworks/example/auth/server/security/AuthSecurityConfig.java
@@ -9,9 +9,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.config.TokenSettings;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.time.Duration;
+import java.util.Arrays;
 
 @EnableWebSecurity
 @Configuration
@@ -32,6 +40,24 @@ public class AuthSecurityConfig {
 
     @Bean
     public RegisteredClientRepository registeredClientRepository(PasswordEncoder passwordEncoder) {
-        return new InMemoryRegisteredClientRepository();
+        RegisteredClient awuserClient = RegisteredClient
+                .withId("1")
+                .clientId("awuser")
+                .clientSecret(passwordEncoder.encode("123456"))
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .scope("users:read")
+                .scope("users:write")
+                .tokenSettings(TokenSettings.builder()
+                        .accessTokenTimeToLive(Duration.ofMinutes(5))
+                        .build())
+                .clientSettings(ClientSettings.builder()
+                        .requireAuthorizationConsent(false)
+                        .build())
+                .build();
+
+        return new InMemoryRegisteredClientRepository(
+                Arrays.asList(awuserClient)
+        );
     }
 }
